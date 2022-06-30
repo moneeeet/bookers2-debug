d07bf24d354e74bb8824b8b47fee5f9228a16019
EC2 Default User <ec2-user@ip-172-31-74-48.ec2.internal>
2022-06-30 05:21:55 +0000

    2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  has_many :book_comments, dependent: :destroy
+
+  def favorited?(user)
+   favorites.where(user_id: user.id).exists?
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "9288950852eb4dec9d6b1c63103c1c2c.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
+
 end
